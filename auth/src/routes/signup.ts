361f17334ccb654b50cf654c96910c4cf1a152b6
yashraj021 <yash.raj021@gmail.com>
2020-07-14 00:58:56 +0530

    Fixed user model folder structure. Added BadRequestHandler error class.

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { User } from '../models/users';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -23,10 +24,18 @@ router.post(
 
     const { email, password } = req.body;
 
-    console.log('Creating user');
-    throw new DatabaseConnectionError();
+    const existingUser = await User.findOne({ email });
 
-    res.send({});
+    if (existingUser) {
+      console.log('Email exists.');
+      throw new BadRequestError('Email in use.');
+    }
+
+    const user = User.build({ email, password });
+
+    await user.save();
+
+    res.status(201).send({ user });
   }
 );
 
