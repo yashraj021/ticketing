a1b5b16c870600fc4c72429bf87ca950a3b5b3b1
yashraj021 <yash.raj021@gmail.com>
2020-07-14 23:41:58 +0530

    Added cookie-session.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { NotFoundError } from './errors/not-found-error';
 import { currentUserRouter } from './routes/current-user';
@@ -12,7 +13,14 @@ import { errorHandler } from './middlewares/error-handler';
 
 const app = express();
 
+app.set('trust proxy', true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(currentUserRouter);
 app.use(singninRouter);
