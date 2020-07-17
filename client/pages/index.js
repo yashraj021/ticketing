02997a17938657ca8cb8ae31c451caa40e618765
yashraj021 <yash.raj021@gmail.com>
2020-07-18 04:07:42 +0530

    Fixing

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,29 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>Hi</h1>;
 };
+
+LandingPage.getInitialProps = async () => {
+  if (typeof window === 'undefined') {
+    const { data } = await axios.get(
+      'http://ingress-nginx-load.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: {
+          Host: 'ticketing.dev',
+        },
+      }
+    );
+
+    return data;
+  } else {
+    const { data } = await axios.get('/api/users/currentuser');
+
+    return data;
+  }
+
+  return {};
+};
+
+export default LandingPage;
