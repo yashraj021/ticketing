b397ca1e3c217d77cf83992703d7ce4b5c26c449
yashraj021 <yash.raj021@gmail.com>
2020-07-22 23:12:20 +0530

    Index route completed.

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -6,6 +6,7 @@ import cookieSession from 'cookie-session';
 
 import { createTicketRouter } from './routes/new';
 import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.use(
 app.use(currentUser);
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
 app.all('*', async () => {
   throw new NotFoundError();
