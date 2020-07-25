c3d276d65fdddf4df35cee9210586bd9cf0f0921
yashraj021 <yash.raj021@gmail.com>
2020-07-26 01:38:58 +0530

    Created enum for Subjects and interface for TicketcreatedListener for strict binding.

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,13 +1,18 @@
 import { Message } from 'node-nats-streaming';
 import { Listener } from './base-listener';
+import { TicketCreatedEvent } from './ticket-created-events';
+import { Subjects } from './subjects';
 
-export class TicketCreatedListener extends Listener {
-  subject = 'ticket:created';
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
   queueGroupName = 'payments-service';
 
-  onMessage(data: any, msg: Message) {
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) {
     console.log('Event data!', data);
 
+    console.log(data.id);
+    console.log(data.price);
+    console.log(data.title);
     msg.ack();
   }
 }
