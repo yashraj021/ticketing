dc359cc1f41259dd3b32acff1db9225205e68d37
yashraj021 <yash.raj021@gmail.com>
2020-07-27 01:49:53 +0530

    Fixed common module typos. Created ticket-updated-publisher.

---
+++ tickets/src/events/publisher/ticket-updated-publisher.ts
@@ -0,0 +1,5 @@
+import { Publisher, Subjects, TicketUpdatedEvent } from '@ystickets/common';
+
+export class TicketUpdatedPublisher extends Publisher<TicketUpdatedEvent> {
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+}
