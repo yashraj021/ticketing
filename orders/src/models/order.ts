efe052415e4823dccd60422c37692df2edb7d739
yashraj021 <yash.raj021@gmail.com>
2020-09-22 03:03:42 +0530

    Fixed broken tests of Order services.

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import { OrderStatus } from '@ystickets/common';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 import { TicketDoc } from './ticket';
 
 interface OrderAttrs {
@@ -14,6 +15,7 @@ interface OrderDoc extends mongoose.Document {
   status: OrderStatus;
   expiresAt: Date;
   ticket: TicketDoc;
+  version: number;
 }
 
 interface OrderModel extends mongoose.Model<OrderDoc> {
@@ -49,6 +51,9 @@ const orderSchema = new mongoose.Schema(
     },
   }
 );
+
+orderSchema.set('versionKey', 'version');
+orderSchema.plugin(updateIfCurrentPlugin);
 
 orderSchema.statics.build = (attrs: OrderAttrs) => {
   return new Order(attrs);
