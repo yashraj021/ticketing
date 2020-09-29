972ba60bf494fe744e9e3f13e72b105a607e3f8f
yashraj021 <yash.raj021@gmail.com>
2020-09-29 22:56:18 +0530

    Added new tests for ticket service.

---
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,33 @@
+import { OrderCreatedEvent, Listener, Subjects } from '@ystickets/common';
+import { queueGroupName } from './queue-group-name';
+import { Message } from 'node-nats-streaming';
+import { Ticket } from '../../models/ticket';
+import { TicketUpdatedPublisher } from '../publisher/ticket-updated-publisher';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    const ticket = await Ticket.findById(data.ticket.id);
+
+    if (!ticket) {
+      throw new Error('Ticket not found');
+    }
+
+    ticket.set({ orderId: data.id });
+
+    await ticket.save();
+
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      price: ticket.price,
+      title: ticket.title,
+      userId: ticket.userId,
+      version: ticket.version,
+      orderId: ticket.orderId,
+    });
+
+    msg.ack();
+  }
+}
