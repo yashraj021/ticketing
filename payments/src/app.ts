368faf3f91a33ce93aadaf4378e4088ff29e0565
yashraj021 <yash.raj021@gmail.com>
2020-10-16 00:16:55 +0530

    Initiated payments service. Created docker image.

---
+++ payments/src/app.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import { NotFoundError, errorHandler, currentUser } from '@ystickets/common';
+import cookieSession from 'cookie-session';
+
+const app = express();
+
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+
+app.use(currentUser);
+
+app.all('*', async () => {
+  throw new NotFoundError();
+});
+app.use(errorHandler);
+
+export { app };
